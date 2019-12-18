3c7b6dac28b09db81ae31ea69024186a1ed199e9
刘医生 <lookinward.qq@gmail.com>
2019-12-18 22:48:30 +0800

    Update html-img.js

--- html-img.js
+++ html-img.js
@@ -4,17 +4,20 @@ document.getElementsByTagName('head')[0].appendChild(html2canvasScript);}
 
 window.GetImg=function(source) {
 GetImgInit()
-source = document.querySelector(source)
-let = imgContainer = document.createElement('img');
-imgContainer.style.position='fixed';
-imgContainer.style.top='0';
-imgContainer.style.left=0;
-imgContainer.style.right=0;
-imgContainer.style.zIndex=100000;
-        html2canvas(source).then(canvas => {
-            var imageData = canvas.toDataURL("image/png");
-            imgContainer.src = imageData;
-            document.body.appendChild(imgContainer);
-            sessionStorage.setItem('imageSrc', imageData);
-        });
+while(!html2canvas){
+    source = document.querySelector(source)
+    let = imgContainer = document.createElement('img');
+    imgContainer.style.position='fixed';
+    imgContainer.style.top='0';
+    imgContainer.style.left=0;
+    imgContainer.style.right=0;
+    imgContainer.style.zIndex=100000;
+            html2canvas(source).then(canvas => {
+                var imageData = canvas.toDataURL("image/png");
+                imgContainer.src = imageData;
+                document.body.appendChild(imgContainer);
+                sessionStorage.setItem('imageSrc', imageData);
+            });
 }
+}
+
